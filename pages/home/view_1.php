a9718975ccc1e6770766c497f42f4e2ab49dcf0a
Mohamed Dardouri <president@mahd.tn>
2015-03-21 23:36:34 +0100

    Plateform init 1

---
+++ pages/home/view_1.php
@@ -0,0 +1,6 @@
+<!-- custom page styles -->
+<link href="<?php echo $url_root;?>/pages/home/style_1.css" type="text/css">
+<!-- content -->
+
+<!-- custom page scripts -->
+<script src="<?php echo $url_root;?>/pages/home/script_1.js" type="text/javascript"></script>
