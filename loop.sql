3f400f64024158f690cd9734c74bff5d1804aed1
Mohamed Dardouri <dmed@mahd.tn>
2015-04-22 23:58:22 +0100

    database edited

--- loop.sql
+++ loop.sql
@@ -1,56 +1,105 @@
--- phpMyAdmin SQL Dump
--- version 4.2.6deb1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.6.19, for debian-linux-gnu (x86_64)
 --
--- Client :  localhost:3306
--- Généré le :  Dim 19 Avril 2015 à 18:55
--- Version du serveur :  5.5.41-MariaDB-1ubuntu0.14.10.1
--- Version de PHP :  5.5.12-2ubuntu4.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: loop
+-- ------------------------------------------------------
+-- Server version	5.6.19-1~exp1ubuntu2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de données :  `loop`
+-- Table structure for table `category`
 --
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(127) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `icon` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `company`
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category_children`
+--
+
+DROP TABLE IF EXISTS `category_children`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category_children` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id_category` bigint(20) DEFAULT NULL,
+  `id_children` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category_children`
+--
+
+LOCK TABLES `category_children` WRITE;
+/*!40000 ALTER TABLE `category_children` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category_children` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `company`
 --
 
 DROP TABLE IF EXISTS `company`;
-CREATE TABLE IF NOT EXISTS `company` (
-`id` bigint(20) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `company` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `slogan` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `description` varchar(4095) COLLATE utf8_unicode_ci DEFAULT NULL,
   `logo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `cover` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `creation_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `creation_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Vider la table avant d'insérer `company`
+-- Dumping data for table `company`
 --
 
-TRUNCATE TABLE `company`;
--- --------------------------------------------------------
+LOCK TABLES `company` WRITE;
+/*!40000 ALTER TABLE `company` DISABLE KEYS */;
+/*!40000 ALTER TABLE `company` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `company_seat`
+-- Table structure for table `company_seat`
 --
 
 DROP TABLE IF EXISTS `company_seat`;
-CREATE TABLE IF NOT EXISTS `company_seat` (
-`id` bigint(20) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `company_seat` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `id_company` bigint(20) DEFAULT NULL,
   `geolocation` varchar(35) COLLATE utf8_unicode_ci DEFAULT NULL,
   `address` varchar(1023) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -59,23 +108,29 @@ CREATE TABLE IF NOT EXISTS `company_seat` (
   `mobile` varchar(35) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(127) COLLATE utf8_unicode_ci DEFAULT NULL,
   `timing` varchar(511) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `creation_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `creation_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Vider la table avant d'insérer `company_seat`
+-- Dumping data for table `company_seat`
 --
 
-TRUNCATE TABLE `company_seat`;
--- --------------------------------------------------------
+LOCK TABLES `company_seat` WRITE;
+/*!40000 ALTER TABLE `company_seat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `company_seat` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `job`
+-- Table structure for table `job`
 --
 
 DROP TABLE IF EXISTS `job`;
-CREATE TABLE IF NOT EXISTS `job` (
-`id` bigint(20) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `job` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `description` varchar(4095) COLLATE utf8_unicode_ci DEFAULT NULL,
   `geolocation` varchar(35) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -85,86 +140,110 @@ CREATE TABLE IF NOT EXISTS `job` (
   `mobile` varchar(35) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(127) COLLATE utf8_unicode_ci DEFAULT NULL,
   `timing` varchar(511) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `creation_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `creation_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Vider la table avant d'insérer `job`
+-- Dumping data for table `job`
 --
 
-TRUNCATE TABLE `job`;
--- --------------------------------------------------------
+LOCK TABLES `job` WRITE;
+/*!40000 ALTER TABLE `job` DISABLE KEYS */;
+/*!40000 ALTER TABLE `job` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `offer`
+-- Table structure for table `offer`
 --
 
 DROP TABLE IF EXISTS `offer`;
-CREATE TABLE IF NOT EXISTS `offer` (
-`id` bigint(20) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `offer` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `id_company` bigint(20) DEFAULT NULL,
   `text` varchar(4095) COLLATE utf8_unicode_ci DEFAULT NULL,
   `image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `creation_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `creation_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Vider la table avant d'insérer `offer`
+-- Dumping data for table `offer`
 --
 
-TRUNCATE TABLE `offer`;
--- --------------------------------------------------------
+LOCK TABLES `offer` WRITE;
+/*!40000 ALTER TABLE `offer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `offer` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `product`
+-- Table structure for table `product`
 --
 
 DROP TABLE IF EXISTS `product`;
-CREATE TABLE IF NOT EXISTS `product` (
-`id` bigint(20) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `id_company` bigint(20) DEFAULT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `description` varchar(4095) COLLATE utf8_unicode_ci DEFAULT NULL,
   `price` float DEFAULT NULL,
   `image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `creation_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `creation_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Vider la table avant d'insérer `product`
+-- Dumping data for table `product`
 --
 
-TRUNCATE TABLE `product`;
--- --------------------------------------------------------
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `service`
+-- Table structure for table `service`
 --
 
 DROP TABLE IF EXISTS `service`;
-CREATE TABLE IF NOT EXISTS `service` (
-`id` bigint(20) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `service` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `id_company` bigint(20) DEFAULT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `description` varchar(4095) COLLATE utf8_unicode_ci DEFAULT NULL,
   `price` float DEFAULT NULL,
-  `creation_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `creation_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Vider la table avant d'insérer `service`
+-- Dumping data for table `service`
 --
 
-TRUNCATE TABLE `service`;
--- --------------------------------------------------------
+LOCK TABLES `service` WRITE;
+/*!40000 ALTER TABLE `service` DISABLE KEYS */;
+/*!40000 ALTER TABLE `service` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `user`
+-- Table structure for table `user`
 --
 
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-`id` bigint(20) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `id_creator` bigint(20) DEFAULT NULL,
   `type` varchar(63) COLLATE utf8_unicode_ci DEFAULT NULL,
   `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -172,129 +251,52 @@ CREATE TABLE IF NOT EXISTS `user` (
   `displayname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(127) COLLATE utf8_unicode_ci DEFAULT NULL,
   `mobile` varchar(35) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `creation_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `creation_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Vider la table avant d'insérer `user`
+-- Dumping data for table `user`
 --
 
-TRUNCATE TABLE `user`;
--- --------------------------------------------------------
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `user_admin`
+-- Table structure for table `user_admin`
 --
 
 DROP TABLE IF EXISTS `user_admin`;
-CREATE TABLE IF NOT EXISTS `user_admin` (
-`id` bigint(20) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_admin` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `id_user` bigint(20) DEFAULT NULL,
   `id_job_company` bigint(20) DEFAULT NULL,
-  `type` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `type` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Vider la table avant d'insérer `user_admin`
+-- Dumping data for table `user_admin`
 --
 
-TRUNCATE TABLE `user_admin`;
---
--- Index pour les tables exportées
---
+LOCK TABLES `user_admin` WRITE;
+/*!40000 ALTER TABLE `user_admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_admin` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Index pour la table `company`
---
-ALTER TABLE `company`
- ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `company_seat`
---
-ALTER TABLE `company_seat`
- ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `job`
---
-ALTER TABLE `job`
- ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `offer`
---
-ALTER TABLE `offer`
- ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `product`
---
-ALTER TABLE `product`
- ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `service`
---
-ALTER TABLE `service`
- ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `user`
---
-ALTER TABLE `user`
- ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `user_admin`
---
-ALTER TABLE `user_admin`
- ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `company`
---
-ALTER TABLE `company`
-MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `company_seat`
---
-ALTER TABLE `company_seat`
-MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `job`
---
-ALTER TABLE `job`
-MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `offer`
---
-ALTER TABLE `offer`
-MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `product`
---
-ALTER TABLE `product`
-MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `service`
---
-ALTER TABLE `service`
-MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `user`
---
-ALTER TABLE `user`
-MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `user_admin`
---
-ALTER TABLE `user_admin`
-MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-04-22 15:32:03
