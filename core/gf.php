4513a98c94c7f0087550acf66eccb890ee0df15d
Mohamed Dardouri <dmed@mahd.tn>
2015-04-28 12:41:13 +0100

    add gf.php

---
+++ core/gf.php
@@ -0,0 +1,18 @@
+<?php
+
+	class gf {
+
+		public static function getClientIP(){
+
+			if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)){
+		        return  $_SERVER["HTTP_X_FORWARDED_FOR"];  
+		    }else if (array_key_exists('REMOTE_ADDR', $_SERVER)) { 
+		        return $_SERVER["REMOTE_ADDR"]; 
+		    }else if (array_key_exists('HTTP_CLIENT_IP', $_SERVER)) {
+		        return $_SERVER["HTTP_CLIENT_IP"]; 
+		    }
+		    return null;
+		}
+
+	}
+?>
