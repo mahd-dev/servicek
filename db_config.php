a73263ba76aa712738f9da41c227c3436092c55f
Mohamed Dardouri <dmed@mahd.tn>
2015-05-12 15:15:15 +0100

    update about text

--- db_config.php
+++ db_config.php
@@ -1,6 +1,6 @@
 <?php
 	$db_server 		= "127.0.0.1";
 	$db_user 		= "root";
-	$db_password 	= "root";
+	$db_password 		= "root";
 	$db_name 		= "servicek";
 ?>
--- pages/about/view_1.php
+++ pages/about/view_1.php
@@ -17,13 +17,13 @@
 				<div class="row margin-bottom-30">
 					<div class="col-md-12" style="font-size:120%">
 						<p>
-							Le site officiel de la société tunisien des services « www.servicek.net » assemble tout les secteurs de services en Tunisie dans un même espace dont il facilite la façon de chercher les différents moyens d’informations sur les divers types de produits ou de services.
+							Le site officiel de la société tunisienne des services « www.Servicek.net » assemble tout les secteurs de services en Tunisie dans un même espace où il permet de faciliter la recherche des différents informations sur les divers types de produits ou des services.
 						</p>
 						<p>
-							Ce site est diriger surtout a tout propriétaire de service, il supervise l’ouverture vers le plus grand nombre possible des clients sur tout le territoire Tunisiens et ce grâce aux différents méthodes de recherche ou il peut être facilement braqué sur l’endroit qu’il est besoin.
+							Ce site est destiné à tout propriétaire de service, il supervise l’ouverture vers le plus grand nombre possible des clients sur tout le territoire tunisien.
 						</p>
 						<p>
-							L’initiative de site s’appuie sur un processus assez innovant en Tunisie pointer essentiellement sur un mode de recherche visant la collecte de chaque catégorie et sous catégorie avec ses compétences et l’endroit ou le propriétaire du service se trouve, ce qui rend le nombre des clients en croissance et par conséquent localiser sur l’artère commerciale au niveau locale, régional et national ou se trouve le client.
+							L’initiative de site s’appuie sur un processus unique de son genre  en Tunisie. On se pointe essentiellement sur un mode de recherche qui regroupe chaque catégorie des propriétaires suivant leurs secteurs et leurs localisations.
 						</p>
 					</div>
 				</div>
