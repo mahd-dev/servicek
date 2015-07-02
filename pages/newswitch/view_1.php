d82d9c993339b9839f1d4bc5dd6ed84856c362b8
Salem MOSBAHI <salemmosbahi@gmail.com>
2015-07-02 15:57:19 +0100

    create newswitch page

---
+++ pages/newswitch/view_1.php
@@ -0,0 +1,44 @@
+<link href="<?php echo url_root;?>/pages/newswitch/style<?php echo (rtl?"-rtl":"");?><?php if(!debug) echo ".min";?>.css" rel="stylesheet" type="text/css">
+<div class="row">
+	<div class="col-md-12">
+		<div class="box">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<div class="list-group">
+						<div class="row-action-primary radio">
+			        <label>
+								<input type="radio" name="type" checked="">
+								<div class="row-content">
+			            <h4 class="list-group-item-heading">Tile with a checkbox in it</h4>
+			            <p class="list-group-item-text">Donec id elit non mi risus varius blandit.</p>
+				        </div>
+							</label>
+			      </div>
+						<div class="list-group-separator"></div>
+						<div class="row-action-primary radio">
+			        <label>
+								<input type="radio" name="type">
+								<div class="row-content">
+			            <h4 class="list-group-item-heading">Tile with a checkbox in it</h4>
+			            <p class="list-group-item-text">Donec id elit non mi risus varius blandit.</p>
+				        </div>
+							</label>
+			      </div>
+						<div class="list-group-separator"></div>
+						<div class="row-action-primary radio">
+		          <label>
+								<input type="radio" name="type">
+								<div class="row-content">
+		            	<h4 class="list-group-item-heading">Tile with a checkbox in it</h4>
+			            <p class="list-group-item-text">Donec id elit non mi risus varius blandit.</p>
+				        </div>
+							</label>
+        		</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script src="<?php echo url_root;?>/pages/newswitch/script_1<?php if(!debug) echo ".min";?>.js" type="text/javascript"></script>
