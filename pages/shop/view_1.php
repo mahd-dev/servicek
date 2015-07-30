0e58becf96a664f28464c49f190d868e6c6ac9a6
Mohamed Dardouri <dmed@mahd.tn>
2015-07-30 09:08:56 +0100

    fix contact modal title

--- pages/shop/view_1.php
+++ pages/shop/view_1.php
@@ -195,7 +195,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h4 class="modal-title">Envoyez un messag à <?php echo $company->name; ?></h4>
+        <h4 class="modal-title">Envoyez un messag à <?php echo $shop->name; ?></h4>
       </div>
       <div class="modal-body">
 				<form id="message_form" method="post">
