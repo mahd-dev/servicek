600345aea304a0cfd5206dc64b0e82bc6c9801b6
Mohamed Dardouri <dmed@mahd.tn>
2015-09-13 23:31:30 +0100

    backup password system

--- core/user.php
+++ core/user.php
@@ -101,7 +101,7 @@
 					break;
 
 					default:
-						$q=$db->query("select ".$name." from user where (id='".$this->id."')");
+						$q=$db->query("select ".$name." from user where (id='".$this->id."')") or die($db->error);
 						$r=$q->fetch_row();
 						return $r[0];
 					break;
